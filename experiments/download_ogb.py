66365654c4443c67bac1af28d04b31e057f9e69b
Daewon Lee <daewon4you@gmail.com>
2023-02-01 13:07:40 +0900

    Revise download_ogb.py

--- experiments/download_ogb.py
+++ experiments/download_ogb.py
@@ -1,5 +1,5 @@
-from ogb.nodeproppred import PygNodePropPredDataset
-from ogb.linkproppred import PygLinkPropPredDataset
+from ogb.nodeproppred import NodePropPredDataset
+from ogb.linkproppred import LinkPropPredDataset
 
 list_nodeproppred_datasets = [
     "ogbn-arxiv",
@@ -18,7 +18,7 @@ list_linkproppred_datasets = [
 droot = "../data/ogb/"
 
 for dataset_name in list_nodeproppred_datasets:
-    dataset = PygNodePropPredDataset(name=dataset_name, root=droot)
+    dataset = NodePropPredDataset(name=dataset_name, root=droot)
 
 for dataset_name in list_linkproppred_datasets:
-    dataset = PygLinkPropPredDataset(name=dataset_name, root=droot)
+    dataset = LinkPropPredDataset(name=dataset_name, root=droot)
